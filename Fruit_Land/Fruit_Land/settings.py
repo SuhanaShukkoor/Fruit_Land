69f53a668d620a22f25e4ecdccfa64a8980fcdbe
Suhana <suhanasuhi22@gmail.com>
2023-04-13 12:43:11 -0700

    admin panel creation,new database created in mysql,defaulted tables in django is migrated to mysql database by typing a command in terminal of vs code,created an superuser in website by using a command in terminal of vscode,after surver running login to the superuser and created users account then updated to staff account created group

--- Fruit_Land/Fruit_Land/settings.py
+++ Fruit_Land/Fruit_Land/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Fruit_Land.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "Fruit_Landdb",
+        'USER' : "root",
+        'PASSWORD' : "mysql123",
+        'HOST' : "localhost"
     }
 }
 
@@ -118,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
+STATIC_ROOT=os.path.join(BASE_DIR,"backup")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
