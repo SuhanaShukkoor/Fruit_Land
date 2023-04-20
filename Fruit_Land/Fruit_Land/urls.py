3bf0899f23ad7153b8f87cb4020abfffefd4e53f
Suhana <suhanasuhi22@gmail.com>
2023-04-20 12:04:28 -0700

    created urls.py in home app username,password,email verification done then registration process completed,newly registered user in admin page.Username and password in login page verification done

--- Fruit_Land/Fruit_Land/urls.py
+++ Fruit_Land/Fruit_Land/urls.py
@@ -15,15 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
-    path('xyz/',views.test1),
-    path('login/',views.login),
-    path('register/',views.register),
-    path('login/loginsub/',views.loginsub),
-    path('register/regsub/',views.regsub)
+    path('',include("home.urls"))
 ]
--- Fruit_Land/home/views.py
+++ Fruit_Land/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -16,8 +17,8 @@ def register(request):
     return render(request,"register.html")
 
 def loginsub(request):
-    uname=request.GET["user"]
-    pname=request.GET["pas"]
+    uname=request.POST["user"]
+    pname=request.POST["pas"]
     u=auth.authenticate(username=uname,password=pname)
     if u:
         auth.login(request,u)
@@ -25,13 +26,25 @@ def loginsub(request):
     return render(request,"login.html",{"val":u})
 
 def regsub(request):
-    Fname=request.GET["fname"]
-    Lname=request.GET["lname"]
-    Uname=request.GET["user"]
-    Email=request.GET["email"]
-    pname=request.GET["pas"]
-    repas=request.GET["repas"]
-    
+    Fname=request.POST["fname"]
+    Lname=request.POST["lname"]
+    Uname=request.POST["user"]
+    Email=request.POST["email"]
+    pname=request.POST["pas"]
+    repas=request.POST["repas"]
+    if pname==repas:
+        if User.objects.filter(username=Uname):
+            msg="username is already taken"
+            return render(request,"test.html",{"val":msg})
+        elif User.objects.filter(email=Email):
+            msg="Email is already taken"
+            return render(request,"test.html",{"val":msg})
+        else:
+            user=User.objects.create_user(first_name=Fname,last_name=Lname,username=Uname,email=Email,password=pname)
+            user.save();
+            return redirect("/")
+    else:
+        msg="Password invalid"
+        return render(request,"test.html",{"val":msg})
 
-    return render(request,"test.html",{"val":Fname})
 
